fcead47e4f9e650ac4e083ade166f77c31f5d03e
Daniel Bailey <dan.bailey@helpforheroes.org.uk>
2021-10-11 00:01:40 +0100

    Added models project with new registration model

---
+++ AFI.Registration.Models/RegistrationModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AFI.Registration.Models
+{
+    public class RegistrationModel
+    {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public string ReferenceNumber { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+    }
+}
